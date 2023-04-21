e81fd0c0f4f74069e0e258ce7a31ced29c21b791
whiteadol <whiteadol3@naver.com>
2023-04-21 11:19:40 +0900

    initial commit

---
+++ src/main/kotlin/com/example/kotlinapimybatis/KotlinApiMybatisApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinapimybatis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinApiMybatisApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinApiMybatisApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinapimybatis/KotlinApiMybatisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinapimybatis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinApiMybatisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
